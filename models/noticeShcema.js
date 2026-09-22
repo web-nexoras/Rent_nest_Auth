a4a70331ca977de5b52123b925dca07621a7cb8b
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-09-22 23:53:01 +0600

    notice update

--- models/noticeShcema.js
+++ models/noticeShcema.js
@@ -1,25 +1,5 @@
 const mongoose = require("mongoose");
 
-const commentSchema = new mongoose.Schema(
-  {
-    tenant: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    text: {
-      type: String,
-      required: [true, "Comment text is required"],
-      trim: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
-  },
-  { _id: true }
-);
-
 const noticeSchema = new mongoose.Schema(
   {
     title: {
@@ -27,18 +7,43 @@ const noticeSchema = new mongoose.Schema(
       required: [true, "Title is required"],
       trim: true,
     },
+
     description: {
       type: String,
       required: [true, "Description is required"],
+      trim: true,
     },
+
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
-    comments: [commentSchema],
+
+    comments: [
+      {
+        tenant: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+
+        text: {
+          type: String,
+          required: [true, "Comment text is required"],
+          trim: true,
+        },
+
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = mongoose.model("Notice", noticeSchema);
